0c1d6102219a735d6175de8fb92b62ee37a8f174
Eduardo <eduarte@gvdasa.com.br>
2022-10-22 14:46:12 -0300

    Configurando tema claro e cores do tema claro

---
+++ src/shared/themes/Dark.ts
@@ -0,0 +1,22 @@
+import { createTheme } from '@mui/material';
+
+export const DarkTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#ab47bc',
+            light: '#df78ef',
+            dark: '#790e8b',
+            contrastText: '#ffffff'
+        },
+        secondary: {
+            main: '#fff176',
+            light: '#ffffa8',
+            dark: '#cabf45',
+            contrastText: '#000000'
+        },
+        background: {
+            default: '#f7f6f3',
+            paper: '#ffffff'
+        }
+    }
+});
